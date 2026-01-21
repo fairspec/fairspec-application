257df249151ded6cad980e6e03f4109628951396
roll <eskarev@gmail.com>
2026-01-21 11:42:07 +0000

    Removed sitemap

--- website/components/layout/Footer.tsx
+++ website/components/layout/Footer.tsx
@@ -1,10 +1,8 @@
 import { Credits } from "./Credits.tsx"
-import { Sitemap } from "./Sitemap.tsx"
 
 export function Footer() {
   return (
     <footer className="flex flex-col gap-4">
-      <Sitemap />
       <Credits />
     </footer>
   )
