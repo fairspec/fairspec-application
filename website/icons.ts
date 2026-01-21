439db7df72efdb7283264b40c6fdb0f4cdf9f833
roll <eskarev@gmail.com>
2026-01-21 16:32:24 +0000

    Added under construction notes

--- website/icons.ts
+++ website/icons.ts
@@ -2,6 +2,7 @@ import { Share2, Upload } from "lucide-react"
 import { detectPlatform } from "#helpers/platform.ts"
 
 export {
+  AlertTriangle as Alert,
   ArrowBigLeftDash as Return,
   Ban as Fault,
   BarChart,
