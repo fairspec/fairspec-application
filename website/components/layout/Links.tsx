fe54507378ed343a16ce658ce4b8d736dc6c68b9
roll <eskarev@gmail.com>
2026-01-22 14:36:44 +0000

    chore: Updated header links style

--- website/components/layout/Links.tsx
+++ website/components/layout/Links.tsx
@@ -4,24 +4,18 @@ import * as icons from "#icons.ts"
 export function Links() {
   return (
     <div className="hidden lg:flex gap-1 items-center">
-      <Link
-        to="/"
-        className="p-2 underline"
-        style={{ color: "rgb(44, 124, 253)" }}
-      >
+      <Link to="/" className="p-2 text-blue-500 hover:text-blue-400">
         <strong>Application</strong>
       </Link>
       <Link
         to="https://fairspec.org"
-        className="p-2"
-        style={{ color: "rgb(44, 124, 253)" }}
+        className="p-2 text-blue-500 hover:text-blue-400"
       >
         <strong>Standard</strong>
       </Link>
       <Link
         to="https://typescript.fairspec.org"
-        className="p-2"
-        style={{ color: "rgb(44, 124, 253)" }}
+        className="p-2 text-blue-500 hover:text-blue-400"
       >
         <strong>TypeScript</strong>
       </Link>
