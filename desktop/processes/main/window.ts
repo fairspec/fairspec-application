98d49f7518659459e684736e7a1cb2a37300e0fa
roll <eskarev@gmail.com>
2026-01-28 20:18:13 +0000

    Support zooming

--- desktop/processes/main/window.ts
+++ desktop/processes/main/window.ts
@@ -18,16 +18,35 @@ export function createWindow() {
     },
   })
 
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.setTitle(settings.APP_NAME)
+    mainWindow.setMenu(null)
+    mainWindow.maximize()
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.on("before-input-event", (event, input) => {
+    if (input.control) {
+      if (input.key === "+" || input.key === "=") {
+        const currentZoom = mainWindow.webContents.getZoomFactor()
+        mainWindow.webContents.setZoomFactor(currentZoom + 0.1)
+        event.preventDefault()
+      } else if (input.key === "-" || input.key === "_") {
+        const currentZoom = mainWindow.webContents.getZoomFactor()
+        mainWindow.webContents.setZoomFactor(currentZoom - 0.1)
+        event.preventDefault()
+      } else if (input.key === "0") {
+        mainWindow.webContents.setZoomFactor(1.0)
+        event.preventDefault()
+      }
+    }
+  })
+
   if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
     mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"])
   } else {
     mainWindow.loadFile(indexPath)
   }
-
-  mainWindow.setTitle(settings.APP_NAME)
-  mainWindow.setMenu(null)
-  mainWindow.maximize()
-  mainWindow.show()
 
   return mainWindow
 }
