53bb894c6e70f9a5aa20c159194fc742016af620
roll <eskarev@gmail.com>
2026-02-03 12:06:01 +0000

    Minor changes

--- website/client.tsx
+++ website/client.tsx
@@ -2,12 +2,12 @@ import { StartClient } from "@tanstack/react-start/client"
 import { StrictMode } from "react"
 import { hydrateRoot } from "react-dom/client"
 import { activateLocale, detectClientLanguage } from "#helpers/locale.ts"
-import { activateTheme, detectTheme } from "#helpers/theme.ts"
+import { activateTheme, detectClientTheme } from "#helpers/theme.ts"
 
 const language = await detectClientLanguage()
 await activateLocale(language.id)
 
-const theme = await detectTheme()
+const theme = await detectClientTheme()
 activateTheme(theme)
 
 hydrateRoot(
