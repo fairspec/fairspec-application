f0acc249e74d917b2eb31c592b54581c1d30d9e5
roll <eskarev@gmail.com>
2026-02-04 11:01:34 +0000

    Removed @compile.ts from desktop

--- desktop/@compile.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv"
-import { execa } from "execa"
-
-process.chdir(import.meta.dirname)
-dotenv.config({ path: ".env.local" })
-const shell = execa({ stdout: ["inherit"], preferLocal: true, shell: true })
-
-await shell`electron-builder`
